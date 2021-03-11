476a7083bb4d37b113c7e0029c641dd65d1c457f
Sean Enck <enckse@voidedtech.com>
2021-03-11 08:48:32 -0500

    deprecating this

--- src/gitdaemon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SRV=/srv/git
-if [ ! -d $SRV ]; then
-    mkdir -p $SRV
-    ln -s /opt/git/public/ $SRV/cgit.voidedtech.com
-fi
-/usr/bin/git daemon --reuseaddr --export-all --interpolated-path=$SRV/%H/\%D
--- src/renew-certbot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/usr/bin/certbot renew --quiet --agree-tos
-systemctl reload nginx.service
